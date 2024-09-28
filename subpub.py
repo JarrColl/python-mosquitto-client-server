09f37bacb624906e4dd5e65d3d6d52fefd86b924
JarrColl <jarronc@protonmail.com>
2024-09-28 21:49:20 +1000

    begin work on subpub

--- subpub.py
+++ subpub.py
@@ -18,6 +18,8 @@ client_id = f"publish-{random.randint(0, 1000)}"
 username = "102988098"
 password = "jarron"
 
+CONNECT_TIMEOUT = 5
+
 pub_topic = sys.argv[1]
 sub_topics = [("public/#", 1), ("102988098/#", 0)]
 
@@ -51,19 +53,12 @@ def connect_mqtt() -> Client:
     return client
 
 
-def publish(client: Client):
-    msg_count = 1
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(pub_topic, msg, qos=1)
-        if result.rc == MQTTErrorCode.MQTT_ERR_SUCCESS:
-            print(f"Send `{msg}` to topic `{pub_topic}`")
-        else:
-            print(f"Failed to send message to topic {pub_topic}")
-        msg_count += 1
-        if msg_count > 5:
-            break
+def publish(client: Client, msg):
+    result = client.publish(pub_topic, msg, qos=1)
+    if result.rc == MQTTErrorCode.MQTT_ERR_SUCCESS:
+        print(f"Send `{msg}` to topic `{pub_topic}`")
+    else:
+        print(f"Failed to send message to topic {pub_topic}")
 
 
 def subscribe(client: Client):
@@ -80,19 +75,38 @@ def subscribe(client: Client):
 
 def run():
     client = connect_mqtt()
-
     error_code = client.loop_start()
-    if error_code != MQTTErrorCode.MQTT_ERR_SUCCESS:
-        raise MQTTException(
-            "Client failed to loop_forever with error code " + str(error_code) + "."
-        )
-
-    publish(client)
     error_code = client.loop_stop()
     if error_code == MQTTErrorCode.MQTT_ERR_INVAL:
         raise MQTTException(
             "Client loop stop was called but there was no running client loop on the thread."
         )
+
+    # Wait for the connection to be acknowledged by the server before starting.
+    time_waiting = 0
+    while True:
+        if client.is_connected():
+            break
+        else:
+            if time_waiting >= CONNECT_TIMEOUT:
+                print("Connection timed out...")
+                return
+            time_waiting += 0.1
+            time.sleep(0.1)
+
+    subscribe(client)
+
+    f = 0
+    # Main loop
+    while True:
+        if f % 2 == 0:
+            publish(client, f"hi: {f}")
+        f += 1
+        time.sleep(1)
+        if f > 30:
+            return
+
+
 
 
 if __name__ == "__main__":
